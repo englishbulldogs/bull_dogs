43d7d36a7f3b881ec685cf8840832b5543ac726f
Derek Duncan <mail-spam@derekduncan.me>
2013-04-17 16:02:12 -0500

    random front photo

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
 	layout 'application'
 	def index
 		@randalbum = Album.order("RANDOM()").first
-		@rand = @randalbum.images.order(rand("RANDOM()")).first
+		@rand = @randalbum.images.order("RANDOM()").first
 	end
 end
