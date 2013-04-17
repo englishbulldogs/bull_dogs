7d8a6810b2697d78730685c32c57d7eb576595d0
Derek Duncan <mail-spam@derekduncan.me>
2013-04-17 15:14:42 -0500

    database.yml change to post

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,4 @@
 class HomeController < ApplicationController
 	layout 'application'
-	def index
-		@randalbum = Album.offset(rand(Album.count)).first
-		@rand = @randalbum.images.offset(rand(@randalbum.images.count)).first
-	end
+	
 end
