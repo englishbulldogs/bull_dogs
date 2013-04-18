9a736be5186c3db7393880b9d88c28047345b691
Derek Duncan <mail-spam@derekduncan.me>
2013-04-17 23:21:13 -0500

    new images

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,7 @@
 class HomeController < ApplicationController
 	layout 'application'
 	def index
-		offset = rand(Album.count)
-		@randalbum = Album.first(:offset => offset)
-		offset2 = rand(@randalbum.images.count)
-		@rand = @randalbum.images.first(:offset => offset2)
+		@randalbum = Album.offset(rand(Album.count)).first
+		@rand = @randalbum.images.offset(rand(@randalbum.images.count)).first
 	end
 end
